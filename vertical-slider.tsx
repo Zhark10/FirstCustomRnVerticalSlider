7fac9295e14d77a49672bc29d104a51c9fef839e
Zhark10 <arkady.zharavin@citronium.biz>
2020-02-08 19:24:18 +0300

    renamed values

--- vertical-slider.tsx
+++ vertical-slider.tsx
@@ -7,154 +7,6 @@ import {
 
 import Animated from 'react-native-reanimated'
 
-type Props = {
-  value: number
-  disabled: boolean
-  min: number
-  max: number
-  onChange: (value: number) => void
-  onComplete: (value: number) => void
-  width: number
-  height: number
-  borderRadius: number
-  maximumTrackTintColor?: string
-  minimumTrackTintColor?: string
-  step?: number
-  animationDuration?: number
-}
-
-export const VerticalSlider: React.FC<Props> = (props) => {
-  let _moveStartValue: any = null
-
-  const [value, setValue] = React.useState(props.value)
-
-  const panResponderInitial = PanResponder.create({
-    onStartShouldSetPanResponder: () => true,
-    onMoveShouldSetPanResponder: () => false,
-    onPanResponderGrant: () => {
-      _moveStartValue = value
-    },
-    onPanResponderMove: (event, gestureState) => {
-      if (props.disabled) {
-        return
-      }
-      const value = _fetchNewValueFromGesture(gestureState)
-      _changeState(value)
-      if (props.onChange) {
-        props.onChange(value)
-      }
-    },
-    onPanResponderRelease: (event, gestureState) => {
-      if (props.disabled) {
-        return
-      }
-      const value = _fetchNewValueFromGesture(gestureState)
-      _changeState(value)
-      if (props.onComplete) {
-        props.onComplete(value)
-      }
-    },
-    onPanResponderTerminationRequest: () => false,
-    onPanResponderTerminate: (event, gestureState) => {
-      if (props.disabled) {
-        return
-      }
-      const value = _fetchNewValueFromGesture(gestureState)
-      _changeState(value)
-      if (props.onComplete) {
-        props.onComplete(value)
-      }
-    },
-  })
-
-  const [panResponder] = React.useState(panResponderInitial)
-
-  const _fetchNewValueFromGesture = (gestureState: any): number => {
-    const { min, max, step, height } = props
-    const ratio = -gestureState.dy / height
-    const diff = max - min
-    if (step) {
-      return Math.max(
-        min,
-        Math.min(
-          max,
-          _moveStartValue + Math.round((ratio * diff) / step) * step
-        )
-      )
-    }
-    const value = Math.max(min, _moveStartValue + ratio * diff)
-    return Math.floor(value * 100) / 100
-  }
-
-  const _changeState = (value: number): void => {
-    setValue(value)
-  }
-
-  React.useEffect(() => {
-    _changeState(value)
-  }, [])
-
-  const {
-    width,
-    height,
-    borderRadius,
-    maximumTrackTintColor,
-    minimumTrackTintColor,
-  } = props
-  return (
-    <View style={[{ height, width, borderRadius }]}>
-      <View
-        style={[
-          styles.container,
-          {
-            height,
-            width,
-            borderRadius,
-            backgroundColor: maximumTrackTintColor,
-          },
-        ]}
-        {...panResponder.panHandlers}
-      >
-        <Animated.View
-          style={[
-            styles.slider,
-            {
-              height: (value * height) / props.max,
-              width,
-              backgroundColor: minimumTrackTintColor,
-            },
-          ]}
-        />
-        <Animated.View
-          style={{
-            bottom: (value * height) / props.max,
-            height: 26,
-            width: 26,
-            borderRadius: 13,
-            borderWidth: 10,
-            backgroundColor: 'green',
-            borderColor: 'blue',
-            position: 'absolute',
-          }}
-        />
-        <View
-          style={{
-            backgroundColor: 'transparent',
-            borderStyle: 'solid',
-            borderLeftWidth: 5,
-            borderRightWidth: 5,
-            borderBottomWidth: 70,
-            borderLeftColor: 'transparent',
-            borderRightColor: 'transparent',
-            borderBottomColor: 'yellow',
-            zIndex: -1,
-          }}
-        />
-      </View>
-    </View>
-  )
-}
-
 const styles = StyleSheet.create({
   ball: {
     position: 'absolute',
@@ -173,3 +25,153 @@ const styles = StyleSheet.create({
     bottom: 0,
   },
 })
+
+type Props = {
+  value: number
+  disabled: boolean
+  min: number
+  max: number
+  onChange: (value: number) => void
+  onComplete: (value: number) => void
+  width: number
+  height: number
+  borderRadius: number
+  maximumTrackTintColor?: string
+  minimumTrackTintColor?: string
+  step?: number
+  animationDuration?: number
+}
+
+export const VerticalSlider: React.FC<Props> = ({
+  borderRadius,
+  disabled,
+  height,
+  max,
+  onChange,
+  onComplete,
+  value: initialValue,
+  width,
+  maximumTrackTintColor,
+  minimumTrackTintColor,
+  min,
+  step,
+}) => {
+  let _moveStartValue: any = null
+
+  const [gestureOffset, setValue] = React.useState(initialValue)
+
+  const _changeState = (currentState: number): void => {
+    setValue(currentState)
+  }
+
+  const _fetchNewValueFromGesture = (gestureState: any): number => {
+    const ratio = -gestureState.dy / height
+    const diff = max - min
+    if (step) {
+      return Math.max(
+        min,
+        Math.min(
+          max,
+          _moveStartValue + Math.round((ratio * diff) / step) * step
+        )
+      )
+    }
+    const value = Math.max(min, _moveStartValue + ratio * diff)
+    return Math.floor(value * 100) / 100
+  }
+
+  const panResponderInitial = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onMoveShouldSetPanResponder: () => false,
+    onPanResponderGrant: () => {
+      _moveStartValue = gestureOffset
+    },
+    onPanResponderMove: (event, gestureState) => {
+      if (disabled) {
+        return
+      }
+      const value = _fetchNewValueFromGesture(gestureState)
+      _changeState(value)
+      if (onChange) {
+        onChange(value)
+      }
+    },
+    onPanResponderRelease: (event, gestureState) => {
+      if (disabled) {
+        return
+      }
+      const value = _fetchNewValueFromGesture(gestureState)
+      _changeState(value)
+      if (onComplete) {
+        onComplete(value)
+      }
+    },
+    onPanResponderTerminationRequest: () => false,
+    onPanResponderTerminate: (event, gestureState) => {
+      if (disabled) {
+        return
+      }
+      const value = _fetchNewValueFromGesture(gestureState)
+      _changeState(value)
+      if (onComplete) {
+        onComplete(value)
+      }
+    },
+  })
+
+  const [panResponder] = React.useState(panResponderInitial)
+
+  return (
+    <View style={[{ height, width, borderRadius }]}>
+      <View
+        style={[
+          styles.container,
+          {
+            height,
+            width,
+            borderRadius,
+            backgroundColor: maximumTrackTintColor,
+          },
+        ]}
+        {...panResponder.panHandlers}
+      >
+        <Animated.View
+          style={[
+            styles.slider,
+            {
+              height: (gestureOffset * height) / max,
+              width,
+              backgroundColor: minimumTrackTintColor,
+            },
+          ]}
+        />
+        <Animated.View
+          style={{
+            bottom: (gestureOffset * height) / max,
+            height: 26,
+            width: 26,
+            borderRadius: 13,
+            borderWidth: 10,
+            backgroundColor: '#fff',
+            borderColor: '#AB9E98',
+            position: 'absolute',
+          }}
+        />
+        <View
+          style={{
+            backgroundColor: 'transparent',
+            borderStyle: 'solid',
+            borderLeftWidth: 5,
+            borderRightWidth: 5,
+            borderBottomWidth: 70,
+            borderLeftColor: 'transparent',
+            borderRightColor: 'transparent',
+            borderBottomColor: '#DDDDDD',
+            zIndex: -1,
+            transform: [{ rotate: '180deg' }],
+          }}
+        />
+      </View>
+    </View>
+  )
+}
